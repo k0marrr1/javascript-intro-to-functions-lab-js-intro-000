577095faeb65c18d3f484fc6fc36bb45997767e8
omar hussain <omar.hussain4991@gmail.com>
2019-09-14 01:07:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string.toUpperCase() !== string) {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }  else {
     return "YES INDEED!"
-}
+}}
